2db0b28ada309b93d1d72fe27c78ca87a9dc3c01
Muhammad Karimov <hey@karimoff.me>
2022-01-07 20:37:07 +0500

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-honey',
-    version='0.1.0',
+    version='0.2.0',
     author='Mukhammad Karimov',
     author_email='hey@karimoff.me',
     maintainer='Mukhammad Karimov',
